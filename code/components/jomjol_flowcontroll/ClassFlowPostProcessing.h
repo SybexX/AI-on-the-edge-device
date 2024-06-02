27aed613b489bc75dc1500fb7408bc27ae90da77
michael <Heinrich-Tuning@web.de>
2024-06-02 16:04:05 +0200

    Update ClassFlowPostProcessing.h

--- code/components/jomjol_flowcontroll/ClassFlowPostProcessing.h
+++ code/components/jomjol_flowcontroll/ClassFlowPostProcessing.h
@@ -23,24 +23,24 @@ protected:
     ClassFlowCNNGeneral* flowAnalog;
     ClassFlowCNNGeneral* flowDigit;    
 
-    string FilePreValue;
+    std::string FilePreValue;
 
     ClassFlowTakeImage *flowTakeImage;
 
     bool LoadPreValue(void);
-    string ShiftDecimal(string in, int _decShift);
+    std::string ShiftDecimal(std::string in, int _decShift);
 
-    string ErsetzteN(string, double _prevalue);
+    std::string ErsetzteN(std::string input, double _prevalue);
     float checkDigitConsistency(double input, int _decilamshift, bool _isanalog, double _preValue);
 
     void InitNUMBERS();
-    void handleDecimalSeparator(string _decsep, string _value);
-    void handleMaxRateValue(string _decsep, string _value);
-    void handleDecimalExtendedResolution(string _decsep, string _value); 
-    void handleMaxRateType(string _decsep, string _value);
-    void handleAnalogDigitalTransitionStart(string _decsep, string _value);
-    void handlecheckDigitIncreaseConsistency(string _decsep, string _value);
-    void handleAllowNegativeRate(string _decsep, string _value);
+    void handleDecimalSeparator(std::string _decsep, std::string _value);
+    void handleMaxRateValue(std::string _decsep, std::string _value);
+    void handleDecimalExtendedResolution(std::string _decsep, std::string _value); 
+    void handleMaxRateType(std::string _decsep, std::string _value);
+    void handleAnalogDigitalTransitionStart(std::string _decsep, std::string _value);
+    void handlecheckDigitIncreaseConsistency(std::string _decsep, std::string _value);
+    void handleAllowNegativeRate(std::string _decsep, std::string _value);
     void handleIgnoreLeadingNaN(std::string _decsep, std::string _value);
     void handleIgnoreAllNaN(std::string _decsep, std::string _value);
     
@@ -54,17 +54,17 @@ public:
 
     ClassFlowPostProcessing(std::vector<ClassFlow*>* lfc, ClassFlowCNNGeneral *_analog, ClassFlowCNNGeneral *_digit);
     virtual ~ClassFlowPostProcessing(){};
-    bool ReadParameter(FILE* pfile, string& aktparamgraph);
-    bool doFlow(string time);
-    string getReadout(int _number);
-    string getReadoutParam(bool _rawValue, bool _noerror, int _number = 0);
-    string getReadoutError(int _number = 0);
-    string getReadoutRate(int _number = 0);
-    string getReadoutTimeStamp(int _number = 0);
+    bool ReadParameter(FILE* pfile, std::string& aktparamgraph);
+    bool doFlow(std::string time);
+    std::string getReadout(int _number);
+    std::string getReadoutParam(bool _rawValue, bool _noerror, int _number = 0);
+    std::string getReadoutError(int _number = 0);
+    std::string getReadoutRate(int _number = 0);
+    std::string getReadoutTimeStamp(int _number = 0);
     void SavePreValue();
-    string getJsonFromNumber(int i, std::string _lineend);
-    string GetPreValue(std::string _number = "");
-    bool SetPreValue(double zw, string _numbers, bool _extern = false);
+    std::string getJsonFromNumber(int i, std::string _lineend);
+    std::string GetPreValue(std::string _number = "");
+    bool SetPreValue(double zw, std::string _numbers, bool _extern = false);
 
     std::string GetJSON(std::string _lineend = "\n");
     std::string getNumbersName();
@@ -73,7 +73,7 @@ public:
 
     std::vector<NumberPost*>* GetNumbers(){return &NUMBERS;};
 
-    string name(){return "ClassFlowPostProcessing";};
+    std::string name(){return "ClassFlowPostProcessing";};
 };
 
 #endif //CLASSFFLOWPOSTPROCESSING_H
