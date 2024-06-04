246872165a35327278906661d86657bd283e6aca
michael <Heinrich-Tuning@web.de>
2024-06-04 02:37:02 +0200

    Update ClassFlow.h
    getReadout() >>> getReadout_FC()

--- code/components/jomjol_flowcontroll/ClassFlow.h
+++ code/components/jomjol_flowcontroll/ClassFlow.h
@@ -21,7 +21,6 @@ struct HTMLInfo
 	std::string filename_org;	
 };
 
-
 class ClassFlow
 {
 protected:
@@ -46,6 +45,7 @@ public:
 	virtual bool ReadParameter(FILE* pfile, string &aktparamgraph);
 	virtual bool doFlow(string time);
 	virtual string getHTMLSingleStep(string host);
+	virtual std::string getReadout_FC();
 	virtual string name(){return "ClassFlow";};
 
 };
