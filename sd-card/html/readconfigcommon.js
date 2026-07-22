6870abe8be3689ce278e7813706fc0fa88981fc1
SybexXx <587201+SybexX@users.noreply.github.com>
2026-07-22 14:12:03 +0200

    Refactor SaveConfigToServer and UpdateConfig functions

--- sd-card/html/readconfigcommon.js
+++ sd-card/html/readconfigcommon.js
@@ -1,166 +1,229 @@
-function SaveConfigToServer(_domainname){
-     // leere Zeilen am Ende löschen
-     var zw = config_split.length - 1;
-	 
-     while (config_split[zw] == "") {
-          config_split.pop();
-     }
+// ============================================================================
+// Configuration Management - Server Communication
+// ============================================================================
 
-     var config_gesamt = "";
-	 
-     for (var i = 0; i < config_split.length; ++i)
-     {
-          config_gesamt = config_gesamt + config_split[i] + "\n";
-     } 
+function SaveConfigToServer(domainname) {
+    // Remove trailing empty lines
+    var zw = config_split.length - 1;
 
-     FileDeleteOnServer("/config/config.ini", _domainname);
-     FileSendContent(config_gesamt, "/config/config.ini", _domainname);          
+    while (zw >= 0 && config_split[zw] == "") {
+        config_split.pop();
+        zw--;
+    }
+
+    var config = "";
+
+    for (var i = 0; i < config_split.length; ++i) {
+        config = config + config_split[i] + "\n";
+    }
+
+    FileDeleteOnServer("/config/config.ini", domainname);
+    FileSendContent(config, "/config/config.ini", domainname);
 }
 
-function UpdateConfig(zw, _index, _enhance, _domainname){
-     var namezw = zw["name"];
-     FileCopyOnServer("/img_tmp/ref_zw.jpg", namezw, _domainname);
-     var namezw = zw["name"].replace(".jpg", "_org.jpg");
-     FileCopyOnServer("/img_tmp/ref_zw_org.jpg", namezw, _domainname);     
+function UpdateConfig(zw, index, enhance, domainname) {
+    var namezw = zw["name"];
+    FileCopyOnServer("/img_tmp/ref_zw.jpg", namezw, domainname);
+    var namezwOrg = zw["name"].replace(".jpg", "_org.jpg");
+    FileCopyOnServer("/img_tmp/ref_zw_org.jpg", namezwOrg, domainname);
 }
+
+// ============================================================================
+// Image Processing
+// ============================================================================
 
 function createReader(file) {
-     var image = new Image();
-	 
-     reader.onload = function(evt) {
-         var image = new Image();
-		 
-         image.onload = function(evt) {
-             var width = this.width;
-             var height = this.height;
-         };
-		 
-         image.src = evt.target.result; 
-     };
-	 
-     reader.readAsDataURL(file);
- }
+    if (typeof FileReader === 'undefined') {
+        console.error("FileReader API not available");
+        return;
+    }
 
+    var reader = new FileReader();
+    var image = new Image();
+
+    reader.onload = function(evt) {
+        var loadedImage = new Image();
+
+        loadedImage.onload = function(evt) {
+            var width = this.width;
+            var height = this.height;
+            console.log("Image loaded: " + width + "x" + height);
+        };
+
+        loadedImage.onerror = function() {
+            console.error("Error loading image");
+        };
+
+        loadedImage.src = evt.target.result;
+    };
+
+    reader.onerror = function() {
+        console.error("Error reading file");
+    };
+
+    reader.readAsDataURL(file);
+}
+
+// ============================================================================
+// Config String Parsing
+// ============================================================================
+
+/**
+ * Parse a line from config file into tokens
+ * Handles multiple formats:
+ *  - key = value
+ *  - key = value1 value2 value3 ...
+ *  - key value1 value2 value3 ...
+ *
+ * Special handling for password fields which may contain delimiters
+ */
 function ZerlegeZeile(input, delimiter = " =\t\r") {
-    var Output = Array(0);
-//          delimiter = " =,\t";
-     
-    /* The input can have multiple formats: 
-     *  - key = value
-     *  - key = value1 value2 value3 ...
-     *  - key value1 value2 value3 ...
-     *  
-     * Examples:
-     *  - ImageSize = VGA
-     *  - IO0 = input disabled 10 false false 
-     *  - main.dig1 28 144 55 100 false
-     * 
-     * This causes issues eg. if a password key has a whitespace or equal sign in its value.
-     * As a workaround and to not break any legacy usage, we enforce to only use the
-     * equal sign, if the key is "password"
-     */
-    if (input.includes("password") || input.includes("Token")) { // Line contains a password, use the equal sign as the only delimiter and only split on first occurrence
+    var output = [];
+
+    // Special handling for password and token fields
+    if (input.includes("password") || input.includes("Token")) {
+        // Line contains sensitive data; use only the first '=' as delimiter
         var pos = input.indexOf("=");
-        delimiter = " \t\r"
-        Output.push(trim(input.substr(0, pos), delimiter));
-        Output.push(trim(input.substr(pos +1, input.length), delimiter));
-    }
-    else { // Legacy Mode
-        input = trim(input, delimiter);
-        var pos = findDelimiterPos(input, delimiter);
-        var token;
-        
-		while (pos > -1) {
-            token = input.substr(0, pos);
-            token = trim(token, delimiter);
-            Output.push(token);
-            input = input.substr(pos+1, input.length);
-            input = trim(input, delimiter);
-            pos = findDelimiterPos(input, delimiter);
+        if (pos > -1) {
+            var delim = " \t\r";
+            output.push(trim(input.substr(0, pos), delim));
+            output.push(trim(input.substr(pos + 1, input.length), delim));
+            return output;
         }
-        
-		Output.push(input);
     }
-     
-    return Output;
-}    
+
+    // Standard parsing
+    input = trim(input, delimiter);
+    var pos = findDelimiterPos(input, delimiter);
+
+    while (pos > -1) {
+        var token = input.substr(0, pos);
+        token = trim(token, delimiter);
+
+        if (token.length > 0) {
+            output.push(token);
+        }
+
+        input = input.substr(pos + 1, input.length);
+        input = trim(input, delimiter);
+        pos = findDelimiterPos(input, delimiter);
+    }
+
+    // Add the last token
+    if (input.length > 0) {
+        output.push(input);
+    }
+
+    return output;
+}
 
 function findDelimiterPos(input, delimiter) {
     var pos = -1;
-    var zw;
-    var akt_del;
-     
+
     for (var anz = 0; anz < delimiter.length; ++anz) {
-        akt_del = delimiter[anz];
-        zw = input.indexOf(akt_del);
-        
-		if (zw > -1) {
+        var aktDel = delimiter[anz];
+        var zw = input.indexOf(aktDel);
+
+        if (zw > -1) {
             if (pos > -1) {
                 if (zw < pos) {
                     pos = zw;
-				}
-            }
-            else {
+                }
+            } else {
                 pos = zw;
-			}
+            }
         }
     }
-    
-	return pos;
+
+    return pos;
 }
 
 function trim(istring, adddelimiter) {
+    // Trim from start
     while ((istring.length > 0) && (adddelimiter.indexOf(istring[0]) >= 0)) {
-        istring = istring.substr(1, istring.length-1);
+        istring = istring.substr(1, istring.length - 1);
     }
-          
-    while ((istring.length > 0) && (adddelimiter.indexOf(istring[istring.length-1]) >= 0)) {
-        istring = istring.substr(0, istring.length-1);
+
+    // Trim from end
+    while ((istring.length > 0) && (adddelimiter.indexOf(istring[istring.length - 1]) >= 0)) {
+        istring = istring.substr(0, istring.length - 1);
     }
 
     return istring;
 }
 
+// ============================================================================
+// Configuration Loading
+// ============================================================================
+
 function getConfig() {
     return config_gesamt;
 }
 
-function loadConfig(_domainname) {
+function loadConfig(domainname) {
     var xhttp = new XMLHttpRequest();
-    
-	try {
-        url = _domainname + '/fileserver/config/config.ini';     
+
+    try {
+        var url = domainname + '/fileserver/config/config.ini';
         xhttp.open("GET", url, false);
         xhttp.send();
-        config_gesamt = xhttp.responseText;
-        config_gesamt = config_gesamt.replace("InitalRotate", "InitialRotate");         // Korrigiere Schreibfehler in config.ini !!!!!
-    } catch (error) {}
-    
-	return true;
+
+        if (xhttp.status >= 200 && xhttp.status < 300) {
+            config_gesamt = xhttp.responseText;
+            // Fix typo in config file (historical correction)
+            config_gesamt = config_gesamt.replace("InitalRotate", "InitialRotate");
+        } else {
+            console.warn("Failed to load config:", xhttp.status, xhttp.statusText);
+        }
+    } catch (error) {
+        console.error("Error loading config:", error);
+    }
+
+    return true;
 }
+
+// ============================================================================
+// Data Conversion
+// ============================================================================
 
 function dataURLtoBlob(dataurl) {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-          bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    
-	while(n--){
+    var arr = dataurl.split(',');
+    var mimeMatch = arr[0].match(/:(.*?);/);
+    var mime = mimeMatch ? mimeMatch[1] : 'application/octet-stream';
+    var bstr = atob(arr[1]);
+    var n = bstr.length;
+    var u8arr = new Uint8Array(n);
+
+    while (n--) {
         u8arr[n] = bstr.charCodeAt(n);
     }
-    
-	return new Blob([u8arr], {type:mime});
-}	
 
-function FileCopyOnServer(_source, _target, _domainname = "") {
-    url = _domainname + "/editflow?task=copy&in=" + _source + "&out=" + _target;
-    var xhttp = new XMLHttpRequest();  
-    
-	try {
-        xhttp.open("GET", url, false);
-        xhttp.send();
-	} catch (error) {}
+    return new Blob([u8arr], { type: mime });
 }
 
-function FileDeleteOnServer(_filename, _domainname = "") {
+// ============================================================================
+// Server File Operations
+// ============================================================================
+
+function FileCopyOnServer(source, target, domainname = "") {
+    var url = domainname + "/editflow?task=copy&in=" + encodeURIComponent(source) + "&out=" + encodeURIComponent(target);
+    var xhttp = new XMLHttpRequest();
+
+    try {
+        xhttp.open("GET", url, false);
+        xhttp.send();
+
+        if (xhttp.status >= 200 && xhttp.status < 300) {
+            console.log("File copied: " + source + " -> " + target);
+        } else {
+            console.warn("Failed to copy file:", xhttp.status, xhttp.statusText);
+        }
+    } catch (error) {
+        console.error("Error copying file:", error);
+    }
+}
+
+function FileDeleteOnServer(filename, domainname = "") {
     var xhttp = new XMLHttpRequest();
     var okay = false;
 
@@ -168,79 +231,96 @@ function FileDeleteOnServer(_filename, _domainname = "") {
         if (xhttp.readyState == 4) {
             if (xhttp.status == 200) {
                 okay = true;
-            } 
-			else if (xhttp.status == 0) {
-				// firework.launch('Server closed the connection abruptly!', 'danger', 30000);
-				// location.reload()
-            } 
-			else {
-				// firework.launch('An error occured: ' + xhttp.responseText, 'danger', 30000);
-				// location.reload()
+                console.log("File deleted: " + filename);
+            } else if (xhttp.status == 0) {
+                console.error("Server closed connection abruptly");
+            } else {
+                console.error("Error deleting file:", xhttp.status, xhttp.responseText);
             }
         }
     };
-     
-	try {
-        var url = _domainname + "/delete" + _filename;
+
+    try {
+        var url = domainname + "/delete" + filename;
         xhttp.open("POST", url, false);
         xhttp.send();
-    } catch (error) {}
+    } catch (error) {
+        console.error("Error deleting file:", error);
+    }
 
     return okay;
 }
 
-function FileSendContent(_content, _filename, _domainname = "") {
-    var xhttp = new XMLHttpRequest();  
+function FileSendContent(content, filename, domainname = "") {
+    var xhttp = new XMLHttpRequest();
     var okay = false;
 
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4) {
             if (xhttp.status == 200) {
                 okay = true;
-            } 
-			else if (xhttp.status == 0) {
-				firework.launch('Server closed the connection abruptly!', 'danger', 30000);
-            } 
-			else {
-				firework.launch('An error occured: ' + xhttp.responseText, 'danger', 30000);
+                console.log("File sent: " + filename);
+            } else if (xhttp.status == 0) {
+                console.error("Server closed connection abruptly");
+                if (typeof firework !== 'undefined') {
+                    firework.launch('Server closed the connection abruptly!', 'danger', 30000);
+                }
+            } else {
+                console.error("Error sending file:", xhttp.status, xhttp.responseText);
+                if (typeof firework !== 'undefined') {
+                    firework.launch('An error occurred: ' + xhttp.responseText, 'danger', 30000);
+                }
             }
         }
     };
 
     try {
-        upload_path = _domainname + "/upload" + _filename;
-        xhttp.open("POST", upload_path, false);
-        xhttp.send(_content);
-    } catch (error) {}
-	
-    return okay;        
+        var uploadPath = domainname + "/upload" + filename;
+        xhttp.open("POST", uploadPath, false);
+        xhttp.send(content);
+    } catch (error) {
+        console.error("Error sending file:", error);
+    }
+
+    return okay;
 }
 
-function MakeRefImageZW(zw, _enhance, _domainname){
-    var _filename = zw["name"].replace("/config/", "/img_tmp/");
-    var url = _domainname + "/editflow?task=cutref&in=/config/reference.jpg&out=" + _filename + "&x=" + zw["x"] + "&y="  + zw["y"] + "&dx=" + zw["dx"] + "&dy=" + zw["dy"];
-     
-    if (_enhance == true){
+// ============================================================================
+// Image Manipulation
+// ============================================================================
+
+function MakeRefImageZW(zw, enhance, domainname) {
+    var filename = zw["name"].replace("/config/", "/img_tmp/");
+    var url = domainname + "/editflow?task=cutref&in=/config/reference.jpg&out=" + encodeURIComponent(filename);
+    url = url + "&x=" + zw["x"] + "&y=" + zw["y"] + "&dx=" + zw["dx"] + "&dy=" + zw["dy"];
+
+    if (enhance === true) {
         url = url + "&enhance=true";
     }
 
-    var xhttp = new XMLHttpRequest();  
-     
+    var xhttp = new XMLHttpRequest();
+
     try {
         xhttp.open("GET", url, false);
         xhttp.send();
-    } catch (error){}
 
-    if (xhttp.responseText == "CutImage Done") {
-        if (_enhance == true) {
-            firework.launch('Image Contrast got enhanced', 'success', 5000);
+        if (xhttp.responseText == "CutImage Done") {
+            if (enhance === true) {
+                if (typeof firework !== 'undefined') {
+                    firework.launch('Image Contrast got enhanced', 'success', 5000);
+                }
+            } else {
+                if (typeof firework !== 'undefined') {
+                    firework.launch('Alignment Marker have been updated', 'success', 5000);
+                }
+            }
+            return true;
+        } else {
+            console.error("Image operation failed:", xhttp.responseText);
+            return false;
         }
-        else {
-            firework.launch('Alignment Marker have been updated', 'success', 5000);
-        }
-        return true;
-    }
-    else {
+    } catch (error) {
+        console.error("Error making reference image:", error);
         return false;
     }
 }
